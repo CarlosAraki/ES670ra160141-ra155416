f6153cd99537501cdd55b03a8219b217d376d4cd
CarlosAraki <carlosarak@gmail.com>
2018-05-22 13:13:33 -0300

    Update measure.c

--- LAB06/Sources/COOLER/measure.c
+++ LAB06/Sources/COOLER/measure.c
@@ -3,32 +3,57 @@
 #include "../PERIPHERAL/es670_peripheral_board.h"
 #include "../LCD/lcd_hal.h"
 
-void counter_init(void){
+
+/* ************************************************ */
+/* Method name:        measure_Init                 */
+/* Method description:Initialize TPM0 and tachometer*/
+/* function  										*/
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void measure_Init(void){
 	/*un-gate clock TPM*/
 
 	SIM_SCGC6 |= SIM_SCGC6_TPM0(CGC_CLOCK_ENABLED);
 
 	/*un-gate clock PTE29*/
 	SIM_SCGC5 |= SIM_SCGC5_PORTE(CGC_CLOCK_ENABLED);
+	/**/
 	PORTE_PCR29 = PORT_PCR_MUX(TACOMETRO_ALT);
 
+	/**/
 	SIM_SOPT4 = SIM_SOPT4_TPM0CLKSEL(SOP4_SEL);
 	SIM_SOPT2 = SIM_SOPT2_TPMSRC(SOP2_SEL);
+
+	/**/
 	TPM0_SC |=TPM_SC_PS(SOP4_SEL);
 	TPM0_SC |=TPM_SC_CMOD(SOP2_SEL);
 
 }
 
-
-int counter_cont(void){
+/* ************************************************ */
+/* Method name:        measure_Cont                 */
+/* Method description: Return TPM0_CNT Value        */
+/* function  										*/
+/* Input params:       n/a                          */
+/* Output params:      int  iCont                   */
+/* ************************************************ */
+int measure_Cont(void){
 	int iCont;
 	iCont = (TPM0_CNT<<0x0fu);
 	iCont = (iCont>>0x0fu);
 	return iCont;
 }
 
-
-void counter_String(int iCont){
+/* ************************************************ */
+/* Method name:        measure_Strin               */
+/* Method description:Transform int iCont in LCD     */
+/* string                                           */
+/* function  										*/
+/* Input params:       int iCont                     */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void measure_String(int iCont){
 	int cFeature;
 
 		if(0 < iCont/1000){

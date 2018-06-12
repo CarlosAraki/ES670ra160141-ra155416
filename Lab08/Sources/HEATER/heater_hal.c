94ec565fb665660774ecbadb5b0490d986f24862
CarlosAraki <carlosarak@gmail.com>
2018-06-11 23:01:22 -0300

    implementação de comunicação do data com a mauina de estados

--- Lab08/Sources/HEATER/heater_hal.c
+++ Lab08/Sources/HEATER/heater_hal.c
@@ -1,14 +1,29 @@
 #include "../PERIPHERAL/es670_peripheral_board.h"
 #include "adc.h"
-#include "lut_adc_3v3.c"
+#include "lut_adc_3v3.c" //Copiar depois pq não dá certo mesmo com ifdef
+
+
+/*****************************/
+/*******Variáveis*************/
+/*****************************/
+struct Data
+{
+	unsigned int  uiBoolean = 0;
+	int iTemp = 0; //inicializada com temp zero
+}stData;
+
+
+
 /* ************************************************** */
-/* Method name: 	   timer_initTPM1AsPWM        */
-/* Method description: configure Timer1 to act as PWM */
+/* Method name: 	 heater_hal_StateMachine      */
+/* Method description:  maquina de estados do sensor  */
+/*			 de temp		      */
 /* Input params:	   n/a 			      */
 /* Outpu params:	   n/a 			      */
 /* ************************************************** */
 void heater_hal_StateMachine(void)
 {
+	//static int timer se for necessário depois
 	static int siEstado = 1;
 	static unsigned int suiTemperature = 0;
 
@@ -30,8 +45,23 @@ void heater_hal_StateMachine(void)
 	case 3:
 		uiTempCelsius = tabela_temp[suiTemperature];
 		siEstado = 1;
+		stData.iTemp =  uiTempCelsius;
 		break;
 
 	}
 
 }
+
+/* ************************************************** */
+/* Method name: 	 heater_hal_getTemp	      */
+/* Method description:  retorna o valor conv na temp  */
+/* Input params:	   n/a 			      */
+/* Outpu params:	   int iTemp		      */
+/* ************************************************** */
+
+int  heater_hal_getTemp()
+{
+	return stData.iTemp;
+}	
+//Criar struct de dados 
+//Criar get_heater_hal
